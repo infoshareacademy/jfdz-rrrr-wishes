c9fbbd28dd6f0341ad700fa6a0649347fc8b77d0
martastachecka <m.m.stachecka@gmail.com>
2016-07-16 16:09:27 +0200

    Dodanie postera w tle ( tag video) gdyby stronka nie mogła załadować video.

--- funkcje.js
+++ funkcje.js
@@ -40,3 +40,22 @@ document.addEventListener('scroll', function() {
 
 
 });
+
+
+//próbna funkcja
+
+$().ready(function() {
+    var sName = "cookiesok";
+    $("#close-cookie-warn").click(function(){
+        var oExpire = new Date();
+        oExpire.setTime((new Date()).getTime() + 3600000*24*365);
+        document.cookie = sName + "=1;expires=" + oExpire;
+        $("#cookie-warn").hide("slow");
+    });
+
+    var sStr = '; '+ document.cookie +';';
+    var nIndex = sStr.indexOf('; '+ escape(sName) +'=');
+    if (nIndex === -1) {
+        $("#cookie-warn").show();
+    }
+});

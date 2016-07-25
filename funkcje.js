93c7bed9e9df9507c7bc97d7586231ba1d42c7ba
Marta Stachecka <marta.stachecka@structview.com>
2016-07-25 17:25:11 +0200

    Utworzenie branch function-content loading Umieściłam tutaj takżę zmianę dotyczącą slider'a w menu. Zdecydowałam się na użycie css i jquery bo to było zdecydowanie "najczystsze rozwiązanie". Komentarze co do rozwiązania na trello.

--- funkcje.js
+++ funkcje.js
@@ -57,3 +57,31 @@ $().ready(function() {
     }
 });
 */
+/* ³adowanie contentu z pomoc¹ fade*///TO DO
+$(window).on("load",function() {
+    $(window).scroll(function() {
+        $(".fade").each(function() {
+            /* Check the location of each desired element */
+var objectBottom = $(this).offset().top + $(this).outerHeight();
+var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+/* If the element is completely within bounds of the window, fade it in */
+if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+    if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+} else { //object goes out of view (scrolling up)
+    if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+}
+});
+}); $(window).scroll(); //invoke scroll-handler on page-load
+});
+//remove fade class from sections if I don't want to use it.
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+},  20000);

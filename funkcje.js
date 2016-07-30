06cdfd9f92a594b807d3da3580f2c444393edfaa
martastachecka <m.m.stachecka@gmail.com>
2016-07-30 14:26:06 +0200

    skrócenie czasu w slajderze

--- funkcje.js
+++ funkcje.js
@@ -9,7 +9,7 @@ closeModal = function() {
 openModal = function() {
     $('#openModal').show();
 };
-/* smooth przewijanie pomiêdzy sekcjami pomija link do otworzenia okienka modal*/
+/* smooth przewijanie pomiï¿½dzy sekcjami pomija link do otworzenia okienka modal*/
 $(function() {
     $('a[href*=#]:not([href=#]):not([href=#openModal])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -24,11 +24,9 @@ $(function() {
         }
     });
 });
-/* zrób transprentne menu podczas przewijania*/
+/* zrï¿½b transprentne menu podczas przewijania*/
 document.addEventListener('scroll', function() {
-
     console.log(window.pageYOffset);
-
     var header = document.getElementsByClassName('page-section-header')[0];
     if (window.pageYOffset == 0) {
         header.classList.add('nowaKlasa');
@@ -66,7 +64,7 @@ setInterval(function() {
         .fadeIn(1000)
         .end()
         .appendTo('#slideshow');
-},  20000);
+},  10000);
 
 document.addEventListener('scroll', function() {
 
